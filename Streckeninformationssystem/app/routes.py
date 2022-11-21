5e6fb3a38fbc7a776ec4d6be322a11d820382ead
juliaHamm <julia-hammer@gmx.at>
2022-11-21 20:41:55 +0100

    Login 1

--- Streckeninformationssystem/app/routes.py
+++ Streckeninformationssystem/app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from Streckeninformationssystem.app import app
+from Streckeninformationssystem.app.forms import LoginForm
 
 
 @app.route('/')
