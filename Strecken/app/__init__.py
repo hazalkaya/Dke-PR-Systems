c5df69252ee13d3c3d84db218d157f009b8f147d
juliaHamm <julia-hammer@gmx.at>
2022-11-21 21:17:07 +0100

    Login 1

--- Strecken/app/__init__.py
+++ Strecken/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from Strecken.config import Config
+from Strecken.app import routes
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from Strecken.app import routes
+
