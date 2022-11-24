ea906adef0974dcad12738d78d2b0b03afad754c
hazalkaya <hazal_kaya_1999@hotmail.de>
2022-11-24 21:18:34 +0100

    LoginXX

--- Flottensystem/app/routes.py
+++ Flottensystem/app/routes.py
@@ -5,20 +5,11 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+    return render_template('index.html')
 
 @app.route('/login')
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', title='Anmelden', form=form)
